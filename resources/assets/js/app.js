438ed1c353ffa0120fb8a80106efb776418a1765
John Derick De Leon <john07@simplexi.com.ph>
2019-03-19 13:38:48 +0800

    done design on login page

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,15 +7,18 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue';
+import Buefy from 'buefy';
+// import 'buefy/dist/buefy.css';
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+Vue.use(Buefy);
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('login-component', require('./components/login.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="hero is-fullheight is-medium">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="columns is-centered is-mobile">
+                    <div class="column is-two-fifths-desktop is-three-fifths-tablet is-full-mobile">
+                        <div class="login-container">
+                            <figure class="logo">
+                                <img src="/img/logo-m2_primary.png">
+                            </figure>
+                            <b-field>
+                                <b-input placeholder="Username"
+                                         icon="account"
+                                         rounded>
+                                </b-input>
+                            </b-field>
+                            <b-field>
+                                <b-input placeholder="Password"
+                                         type="password"
+                                         icon="lock"
+                                         rounded
+                                         password-reveal>
+                                </b-input>
+                            </b-field>
+                            <a class="button is-primary is-fullwidth">Login</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="columns is-full is-mobile is-centered">
+                    <p>
+                        No account, signup here.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
